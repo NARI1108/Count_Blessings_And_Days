ba5cab21cd1996d807bcb762cfd8bd90b58402b8
Hill <125409221+NARI1108@users.noreply.github.com>
2023-07-20 02:04:38 +0330

    Creating a dedicated action bar and connecting each of the views to its Java layer.

--- app/src/main/java/com/example/countblessingsandweeks/MainActivity.java
+++ app/src/main/java/com/example/countblessingsandweeks/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.countblessingsandweeks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_count_blessings, btn_count_weeks, btn_exit;
+    TextView txt_title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+    public void findViews(){
+        btn_count_blessings =findViewById(R.id.btn_count_blessings);
+        btn_count_weeks = findViewById(R.id.btn_count_weeks);
+        btn_exit = findViewById(R.id.btn_exit);
+        txt_title = findViewById(R.id.txt_title);
     }
 }

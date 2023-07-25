94afc5131472dde585a51be66b7c80474fc27600
Hill <125409221+NARI1108@users.noreply.github.com>
2023-07-25 11:49:04 +0330

    Connecting an XML layer of the activity of counting blessings to its Java layer. The onClickBlessings method is actually when we click on the button and then the number of blessings is shown in the box. Displays their number(result()).

--- app/src/main/java/com/example/countblessingsandweeks/blessing_Activity.java
+++ app/src/main/java/com/example/countblessingsandweeks/blessing_Activity.java
@@ -3,12 +3,36 @@ package com.example.countblessingsandweeks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class blessing_Activity extends AppCompatActivity {
-
+    ImageButton image_button;
+    TextView txt_show;
+    int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blessing);
+        findViews();
+        onClickBlessing();
+    }
+    public void findViews(){
+        image_button = findViewById(R.id.image_button);
+        txt_show = findViewById(R.id.txt_show);
+    }
+    public void onClickBlessing(){
+        image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               result();
+               txt_show.setText(count+"");
+            }
+        });
+    }
+    public void result() {
+        count++;
     }
 }
